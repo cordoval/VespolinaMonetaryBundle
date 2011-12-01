5795ebcfb533ba5065116793a281427ec990fd64
Luis Cordova <cordoval@gmail.com>
2011-12-01 18:03:22 -0500

    removed vendors

--- vendor/doctrine-couchdb/sandbox/cli-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once "bootstrap.php";
-
-$helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
-    'couchdb' => new \Doctrine\CouchDB\Tools\Console\Helper\CouchDBHelper(null, $dm),
-));
--- vendor/doctrine-couchdb/tests/Doctrine/Tests/ODM/CouchDB/CouchDBTestCase.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Doctrine\Tests\ODM\CouchDB;
-
-abstract class CouchDBTestCase extends \PHPUnit_Framework_TestCase
-{
-    
-}
