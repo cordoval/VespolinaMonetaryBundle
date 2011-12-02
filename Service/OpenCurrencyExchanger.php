f2c7200c3c91e8701878d6bfdaa17246cd91feb8
Luis Cordova <cordoval@gmail.com>
2011-12-02 16:56:17 -0500

    added open rates support and set it by default --- google one not working added monetary.xml configuration to adapt this change

---
+++ Service/OpenCurrencyExchanger.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * (c) 2011 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Vespolina\MonetaryBundle\Service;
+
+use Vespolina\MonetaryBundle\Model\CurrencyInterface;
+use Vespolina\MonetaryBundle\Service\CurrencyExchanger;
+ 
+/**
+ * @author Richard Shank <develop@zestic.com>
+ * @author Luis Cordova <cordoval@gmail.com>
+ */
+class OpenCurrencyExchanger extends CurrencyExchanger
+{
+    /**
+     * @inheritdoc
+     */
+    public function getExchangeRate($from, $to, \DateTime $datetime=null)
+    {
+        $from = $this->extractCode($from);
+        $to = $this->extractCode($to);
+        $url = "http://openexchangerates.org/latest.php";
+        $openObject = json_decode(file_get_contents($url));
+        $to = $openObject->rates->$to; // EUR
+        $from = $openObject->rates->$from; // PEN
+        $rate = bcdiv($from, $to, 8); // PEN/USD (26) / EUR/USD (7)
+        return $rate;
+    }
+}
